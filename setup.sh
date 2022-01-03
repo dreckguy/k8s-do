a1acc66df119f30d738eb508f205f45828075b9e
ophir.h <ophir.h@quali.com>
2022-01-03 11:46:20 +0200

    add metallb values file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 # Install Docker
 apt update -y
-# sudo apt install apt-transport-https ca-certificates curl software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-# apt-cache policy docker-ce
-# sudo apt install docker-ce
-# sudo usermod -aG docker ${USER}
-# docker run hello-world
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+apt install docker-ce
+usermod -aG docker ubuntu
+docker run hello-world
